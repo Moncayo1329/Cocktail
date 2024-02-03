4b03094326a01a8ef0559ee3a8db2b1347bdc990
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-02-03 19:07:11 +0000

    add

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Navbar = () => {
+return (
+
+<nav className="navbar">
+    <div className="nav-center">
+<Link to="/">
+
+
+</Link>
+
+    </div>
+
+<h2>Navbar Component</h2>
+
+</nav>
+
+
+)
+
+}
+
+
+export {Navbar}
