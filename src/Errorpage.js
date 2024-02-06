e69ebd22cfc6f4899da0d204c5f892c3a394da8f
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2024-02-06 18:43:34 +0000

    fixing bugs

---
+++ src/Errorpage.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Error = () => {
+
+return (
+
+<section className="error-page section">
+<div className="error-container">
+<h1>opsss! it is dead af</h1>
+<Link to="/" className="btn btn-primary">
+
+back Home
+</Link>
+</div>
+</section>
+
+
+)
+} 
+
+export {Error}
